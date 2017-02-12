e1e057508ea831abda4eb994be4a2f68b456d819
Jon Zachary <jontzachary@gmail.com>
2017-02-11 19:54:17 -0500

    add dark sky api call returns current temp

--- routes/search.js
+++ routes/search.js
@@ -4,19 +4,23 @@ var axios = require('axios');
 
 
 router.get('/', function(req,res,next){
-const google_key=process.env.GOOGLE_KEY;
+const google_key=process.env.GOOGLE_KEY; 
+const dark_sky_key=process.env.DARK_SKY_KEY;
 const val = req.query.location;
-console.log(val);
 const url = 'https://maps.googleapis.com/maps/api/geocode/json?address='+ val+'&key=' + google_key;
-
 axios.get(url)
 .then(function(response) {
   const coords ={lat:response.data.results[0].geometry.location.lat,
                  lng:response.data.results[0].geometry.location.lng};
-                 console.log(coords.lat);
+                 res.locals.coords=coords;
   res.render("index",{title: "WeatherExpress",coords:coords});
+  const url2='https://api.darksky.net/forecast/e65e7156ea5f28a13d13f2e0f05128c2/'+ coords.lat + ',' +coords.lng;
+  axios.get(url2)
+  .then(function(response) {
+    console.log(response.data.currently.temperature);
   });
-    
+
+  });
 });
 
 
