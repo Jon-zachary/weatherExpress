e07179b03eb6b7a02ea76bcd2fa856c858fd3bcd
Jon Zachary <jontzachary@gmail.com>
2017-03-04 16:54:45 -0500

    who knows

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,12 @@
-var axios = require('axios');
 
 function useCurrentLoc() {
  document.querySelector('#location').value='Loading...';
-  navigator.geolocation.getCurrentPosition(success);
+  navigator.geolocation.getCurrentPosition(success());
 }
 
 function success(position) {
    const pos = position.coords;
+   console.log(pos);
    document.querySelector('#location').value=pos.latitude+","+pos.longitude;
    document.forms["searchForm"].submit();
 }
