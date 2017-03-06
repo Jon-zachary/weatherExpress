28dd54d4b8e67acbfaa12fc37ea850fae226a701
Jon Zachary <jontzachary@gmail.com>
2017-03-06 01:06:23 -0500

    not sure

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,9 +3,8 @@ function success(position) {
    const pos = position.coords;
    document.querySelector('#location').value=pos.latitude+","+pos.longitude;
    document.forms["searchForm"].submit();
-
 }
+
 function useCurrentLoc() {
- 
   navigator.geolocation.getCurrentPosition(success);
 }
